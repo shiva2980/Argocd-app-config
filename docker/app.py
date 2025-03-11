597d88d8887679779b45dcafc7470edbd3e1179c
Shiva Reddy <shivadevops2021@gmail.com>
2025-03-11 22:30:54 +0530

    Added changes

--- docker/app.py
+++ docker/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello,! This is a simple Flask app running in Docker with image3:1.2."
+    return "Hello,! This is a simple Flask app running in Docker with imagethree:3.0"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002)
+    app.run(host='0.0.0.0', port=5004)

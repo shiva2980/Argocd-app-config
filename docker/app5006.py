a281adef3e1ab4ab3e3f8a84aef055c97aca31aa
“[Shiva <shivadevops2021@gmail.com>
2025-05-21 13:30:03 +0530

    updated

---
+++ docker/app5006.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello,! This is a simple Flask app running in Docker with imageone:1.0"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5006)
